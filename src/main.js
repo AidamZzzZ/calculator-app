98f0e16e57f10c45c74fdd10baac8eaaad0fa97c
AidamZzzZ <adrian_alliskair@hotmail.com>
2025-03-04 19:56:16 -0400

    last commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const numberEl = document.querySelector("#display");
+const keysEl = document.querySelectorAll("input");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+keysEl.forEach((key) => {
+  key.addEventListener("click", () => {
+    if (key.value === "=") {
+      numberEl.value = eval(numberEl.value);
+    } else if (key.value ===  "C") {
+      numberEl.value = "";
+    } else if (key.value === "<-"){ 
+      numberEl.value = numberEl.value.slice(0, -1)
+    } else {
+      numberEl.value += key.value;
+    }
+  })
+})
